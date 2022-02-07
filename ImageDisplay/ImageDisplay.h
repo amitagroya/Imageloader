b90cc0462bdf940e900983889ffa68612fc6ee2e
Amit Soni <amitksoni.it@gmail.com>
2022-02-07 19:02:40 +0530

    Initial Commit

---
+++ ImageDisplay/ImageDisplay.h
@@ -0,0 +1,18 @@
+//
+//  ImageDisplay.h
+//  ImageDisplay
+//
+//  Created by Agroya on 07/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ImageDisplay.
+FOUNDATION_EXPORT double ImageDisplayVersionNumber;
+
+//! Project version string for ImageDisplay.
+FOUNDATION_EXPORT const unsigned char ImageDisplayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageDisplay/PublicHeader.h>
+
+
